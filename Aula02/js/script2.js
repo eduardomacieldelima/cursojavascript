ded596380a184413677326172b9bb1f7549760b6
Eduardo Maciel <eduardomacieldelima@hotmail.com>
2023-01-24 23:34:14 -0300

    Inclusão de atividades de treino em casa

--- Aula02/js/script2.js
+++ Aula02/js/script2.js
@@ -3,9 +3,26 @@
 // var foo = "bar";
 // console.log(foo);
 
+// var estaVarialvel = 'primeiro valor';
+// var aquelaVariavel = 'segundo valor';
+// console.log("Esta Variável:", estaVarialvel, "e aquela variável: ", aquelaVariavel);
 
+// console.log("Olá Mundo!");
+// var x = 10;
+// var y = 20;
+// var sum = x + y;
+// console.log(sum);
 
+// var greet = "Olá", who = "Mundo";
+// console.log("%s, %s!", who, greet);
 
-
-var greet = "Olá", who = "Mundo";
-console.log("%s, %s!", who, greet);
+console.log({
+'Email': '',
+'Groups':{},
+'Id':33,
+'IsSiteAdmin':false,
+'IsHiddenInUI':false,
+'LoginName': 'i:0#.w|virtualdomain\\user2',
+'PrincipalType': 1,
+'Title': 'user2',
+});
