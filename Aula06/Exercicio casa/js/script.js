c7ef99a7a7d5af08750165a02f96bf7e02e5c967
Eduardo Maciel <eduardomacieldelima@hotmail.com>
2023-02-09 13:38:48 -0300

    Exercicio de casa que Faltou dar git push

---
+++ Aula06/Exercicio casa/js/script.js
@@ -0,0 +1,15 @@
+window.document.head.innerHTML = `<link rel="stylesheet" href="css/styles.css">`;
+
+const body = window.document.body;
+
+function paginaProdutos(){
+    const header = document.createElement('header');
+    const h1 = document.createElement('h1');
+    h1.innerHTML = 'Lista de Produtos';
+    header.appendChild(h1);
+
+    const main = document.createElement('main');
+
+    var pos = 0;
+    
+}
